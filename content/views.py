55bc15d819ce8024ff88fa41bcba3d9c2c40b92e
Adam <adam_ac_@hotmail.com>
2021-01-08 08:03:40 +0000

    Adding titles to generic views

--- content/views.py
+++ content/views.py
@@ -13,12 +13,26 @@ class ProductsListView(ListView):
     context_object_name = 'products'
     ordering = ['name']
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Product List'
+        return context
+
 
 class ProductDetailView(DetailView):
     model = Product
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Product Detail'
+        return context
+
 
 def contact(request):
-    return render(request, 'content/contact.html', {'title': 'Contact'})
+    return render(request, 'content/contact.html', {'title': 'Contact Us'})
+
+
+def about(request):
+    return render(request, 'content/about.html', {'title': 'About Us'})
 
 
